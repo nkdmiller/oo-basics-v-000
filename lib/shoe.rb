56d25881073cea226acd7d9fdb6407dc74ce97c1
Nate Miller <natekmiller96@gmail.com>
2018-06-26 18:55:55 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,8 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+
+  def cobble
+    puts "Shoe Repaired"
+    Shoe.condition = "new"
+  end
+end
