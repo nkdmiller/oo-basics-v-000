5bd63c689a837a31e0e586a337ca75f8ec60e5e9
Nate Miller <natekmiller96@gmail.com>
2018-06-26 18:52:28 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -22,6 +22,13 @@ class Book
     @page_count
   end
 
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
